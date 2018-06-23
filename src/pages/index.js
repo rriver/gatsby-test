079ac7d6f36ff2f2e065fc08b224c993c8b7da3c
Ryo Watanabe <ryo.watanabe@gmail.com>
2018-06-23 22:03:29 +0900

    update top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () =>
   <div>
     <h1 style={{ color: `tomato` }}>Gatsby to Netlify Deploy Test!</h1>
     <p>Testing GatsbyJS to see if it makes sense to use it.</p>
-    <p>I am not sure if I need to buid on the local env first to have it deployed.</p>
+    <p>I am not sure if I need to buid on the local env first to have it deployed. Turned out I don't.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
     <ul>
