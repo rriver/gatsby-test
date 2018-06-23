abb39c696bb005a0d650bc8ababf0ca55f6fe589
Ryo Watanabe <ryo.watanabe@gmail.com>
2018-06-23 21:57:30 +0900

    gatsby without build

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import Link from "gatsby-link";
 
 export default () =>
   <div>
-    <h1 style={{ color: `tomato` }}>Hello Gatsby!</h1>
-    <p>This is the first project.</p>
+    <h1 style={{ color: `tomato` }}>Gatsby to Netlify Deploy Test!</h1>
+    <p>Testing GatsbyJS to see if it makes sense to use it.</p>
+    <p>I am not sure if I need to buid on the local env first to have it deployed.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
     <ul>
